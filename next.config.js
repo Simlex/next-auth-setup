87c45d6988e05bf68552eecb39b2d7d73ce2cd34
Simlex <simlexafol@gmail.com>
2023-11-20 08:44:43 +0100

    Add experimental serverActions to next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        serverActions: true
+    },
+    // env: {
+    //     GOOGLE_CLIENT_ID: '435839482634-5a8gn4uveahv46cmk7dmb20q1ip1dt45.apps.googleusercontent.com',
+    //     GOOGLE_CLIENT_SECRET: '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'
+    // },
+}
+
+module.exports = nextConfig
