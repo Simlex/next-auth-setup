e87864dc8cf78b10f439c8f4af28d0a53130954e
Simlex <simlexafol@gmail.com>
2023-11-20 08:45:48 +0100

    Add image imports to images.ts

---
+++ public/images.ts
@@ -0,0 +1,8 @@
+import logo from "./images/xtechhub_logo.png";
+import user_avatar from "./images/user_avatar.png";
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  logo,
+  user_avatar,
+};
